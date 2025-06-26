3058ae21fd7939fca2badda75d88f6392e902ead
AndreiPC <andreipatea@gmail.com>
2025-06-26 12:46:06 +0100

    Add project files.

---
+++ BatchImageGrayscale/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BatchImageGrayscale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string originFolder;
+            string destinationFolder;
+
+            if (args.Length == 2)
+            {
+                originFolder = args[0];
+                destinationFolder = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Usage: BatchImageGrayscale <originFolder> <destinationFolder>");
+                return;
+            }
+
+            if (!Directory.Exists(originFolder))
+            {
+                Console.WriteLine("Directory does not exist: " + originFolder);
+                return;
+            }
+
+            if (!Directory.Exists(destinationFolder))
+            {
+                // Create directory
+                Directory.CreateDirectory(destinationFolder);
+            }
+
+            DirectoryInfo info = new DirectoryInfo(originFolder);
+
+            int totalFiles = info.GetFiles().Length;
+            int doneCounter = 0;
+
+            foreach (var file in info.EnumerateFiles())
+            {
+                Console.WriteLine();
+                Console.Write("Converting " + file.Name + "...");
+
+                Bitmap bitmap = new Bitmap(file.FullName);
+
+                try
+                {              
+                    bitmap = ConvertFileToGrayscale(bitmap);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Couldn't parse file " + file.Name + ". Error: " + ex.Message);
+                }             
+
+                // Save the image
+
+                string destinationFilePath = Path.Combine(destinationFolder, file.Name);
+                bitmap.Save(destinationFilePath, ImageFormat.Png);
+
+                doneCounter++;
+
+                Console.Write(" done. " + doneCounter + "/" + totalFiles + " files processed.");
+            }
+        }
+
+        private static Bitmap ConvertFileToGrayscale(Bitmap bitmap)
+        {
+            Bitmap grayBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+            // Convert to grayscale
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    Color grayColor = Color.FromArgb(pixelColor.R, pixelColor.R, pixelColor.R);
+                    grayBitmap.SetPixel(x, y, grayColor);
+                }
+            }
+
+            bitmap = grayBitmap;
+            return bitmap;
+        }
+    }
+}
